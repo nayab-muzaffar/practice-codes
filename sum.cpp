6ff9cce0f5784bd137fe23905ad161898ca4ea30
nayab-muzaffar <nayabmuzaffar3@gmail.com>
2026-08-26 20:06:48 -0700

    trying git commands

---
+++ sum.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a=34,b=2;
+    int sum=a+b;
+    cout<<"Trying git command"<<endl;
+    cout<<"Sum of a "<<a<<" and b "<<b<<" is "<<sum<<endl;
+    return 0;
+}
